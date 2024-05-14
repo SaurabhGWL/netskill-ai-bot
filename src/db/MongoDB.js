a728edabca475ffa4946add2eae73396dbd3c4bd
saurabh-apty <138441014+saurabh-apty@users.noreply.github.com>
2024-05-14 22:06:13 +0530

    update env variable

--- src/db/MongoDB.js
+++ src/db/MongoDB.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const uri = process.env.MONGO_URL;
+const uri = process.env.MONGODB_URI;
 
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
