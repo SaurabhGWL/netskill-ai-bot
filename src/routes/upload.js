6f8333303d4a9373c95fb0f18022560a5fad3d1e
saurabh-apty <138441014+saurabh-apty@users.noreply.github.com>
2024-06-14 12:39:41 +0530

    update chatbot version

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,55 +1,65 @@
 import express from "express";
-import multer from 'multer';
+import multer from "multer";
 import UploadedDocument from "../models/DocumentUpload.js";
 import { createEmbedding } from "../utils/createEmbedding.js";
 import { PdfReader } from "pdfreader";
 
-
 const upload = multer();
 const router = express.Router();
 
-router.post("/", upload.single('pdf'), async (req, res) => {
-    let { user_id } = req.body;
-    console.log('check user id', user_id);
-    try {
-      if (!req.file) {
-        return res.status(400).json({ error: 'No file was uploaded.' });
-      }
-  
-      const pdfFile = req.file;
-      console.log('check pdf file', pdfFile);
-      const text = await processPDFStream(pdfFile.buffer);
-      // const embedding = await createEmbedding(text);
-      const newDoc = new UploadedDocument({
-        description: text,
-        // embedding: embedding,
-        user_id:user_id
-      });
-  
-      const savedDoc = await newDoc.save();
-      res.status(201).json({
-        message: "Document uploaded successfully",
-        document: savedDoc,
-      });
-    } catch (err) {
-      console.error("Error:", err);
-      res.status(500).json({ error: "Internal server error" });
+router.post("/", upload.single("pdf"), async (req, res) => {
+  let { user_id } = req.body;
+  console.log("check user id", user_id);
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file was uploaded." });
     }
-  });
-  
-  async function processPDFStream(pdfBuffer) {
-    return new Promise((resolve, reject) => {
-      const textChunks = [];
-  
-      new PdfReader().parseBuffer(pdfBuffer, (err, item) => {
-        if (item?.text) {
-          textChunks.push(item.text);
-        } else if (err) {
-          console.log("Error Parsing!");
-          reject(err);
-        } else if (!item) resolve(textChunks.join(""));
-      });
+
+    const pdfFile = req.file;
+    console.log("check pdf file", pdfFile);
+    const text = await processPDFStream(pdfFile.buffer);
+    // const embedding = await createEmbedding(text);
+    const newDoc = new UploadedDocument({
+      description: text,
+      fileName: pdfFile?.originalname,
+      user_id: user_id,
     });
+
+    const savedDoc = await newDoc.save();
+    res.status(201).json({
+      message: "Document uploaded successfully",
+      document: savedDoc,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json({ error: "Internal server error" });
   }
+});
+
+async function processPDFStream(pdfBuffer) {
+  return new Promise((resolve, reject) => {
+    const textChunks = [];
+
+    new PdfReader().parseBuffer(pdfBuffer, (err, item) => {
+      if (item?.text) {
+        textChunks.push(item.text);
+      } else if (err) {
+        console.log("Error Parsing!");
+        reject(err);
+      } else if (!item) resolve(textChunks.join(""));
+    });
+  });
+}
+
+router.get("/documents", async (req, res) => {
+  try {
+    const userId = "EUREKA";
+    const documents = await UploadedDocument.find({ user_id: userId });
+    res.status(200).json(documents);
+  } catch (err) {
+    console.error("Error fetching documents:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export default router;
--- src/utils/createChat.js
+++ src/utils/createChat.js
@@ -6,23 +6,35 @@ const openai = new OpenAI({
 });
 
 async function PromptResponse(prompt) {
+  // const response = await openai.chat.completions.create({
+  //   model: "gpt-3.5-turbo",
+  //   stream: false,
+  //   temperature: 0.5,
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content: "You are a helpful assistant.",
+  //     },
+  //     {
+  //       role: "user",
+  //       content: prompt,
+  //     },
+  //   ],
+  // });
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    stream: false,
-    temperature: 0.5,
     messages: [
       {
         role: "system",
-        content: "You are a helpful assistant.",
+        content: "You are a helpful assistant designed to output JSON.",
       },
-      {
-        role: "user",
-        content: prompt,
-      },
+      { role: "user", content: prompt },
     ],
-  });
+    model: 'gpt-4o',
+    response_format: { type: "json_object" }
+  })
 
-  return response?.choices[0];
+  return response?.choices[0].message.content
+  // return response?.choices[0];
 }
 
 export default PromptResponse;
